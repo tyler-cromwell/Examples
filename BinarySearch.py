ee8128d3b45e984ae1777d80f93db54523b42aa1
Tyler Cromwell <tjc6185@gmail.com>
2017-12-21 15:19:56 -0500

    Added Binary Search example

---
+++ BinarySearch.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import random
+
+from common import Node
+
+
+def binary_search(sl, data):
+    start = 0
+    mid = len(sl) // 2
+    end = len(sl)
+
+    while end != start:
+        if data > sl[mid]:
+            start = mid
+        elif data < sl[mid]:
+            end = mid
+        else:
+            return mid
+
+        mid = ((end - start) // 2) + start
+
+    return -1
+
+
+if __name__ == '__main__':
+    s = 10000
+    l = [random.randint(0, s) for i in range(s)]
+
+    print(l)
+    l.sort()
+    print(l)
+
+    for e in l:
+        print('Searching for', e, ':', binary_search(l, e))
