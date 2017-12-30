b85e4c75908cfd5930eca15c1cb9fe08daf6f3fb
Tyler Cromwell <tjc6185@gmail.com>
2017-12-30 11:08:43 -0500

    Added Insertion Sort example

--- Sort.py
+++ Sort.py
@@ -17,11 +17,26 @@ def selection_sort(array):
     pass
 
 
-def insertion_sort(array):
-    pass
+def insertion_sort(data):
+    array = data[:]
+    i = 1
+
+    while i < len(array):
+        j = i
+
+        while j > 0 and array[j-1] > array[j]:
+            temp = array[j-1]
+            array[j-1] = array[j]
+            array[j] = temp
+            j -= 1
+
+        i += 1
+
+    return array
 
 
 if __name__ == '__main__':
     l = [5, 2, 3, 6, 1, 0, 4]
     print(l)
     print(bubble_sort(l))
+    print(insertion_sort(l))
