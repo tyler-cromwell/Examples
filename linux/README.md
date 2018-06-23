971b312e81f2858f9c58472c1d12f9ddb7c9d5a9
Tyler Cromwell <tjc6185@gmail.com>
2018-06-23 13:00:10 -0400

    Added linux kernel module example

---
+++ linux/README.md
@@ -0,0 +1,17 @@
+# Linux
+Example of a basic [Linux][linux] [kernel module][module].
+
+To build and install, execute [make] and insert the module object file (`.ko`):
+```
+$ make
+$ sudo insmod linuxmod.ko
+```
+
+Remove module using just the name:
+
+`$sudo rmmod linuxmod`.
+
+
+[linux]: https://en.wikipedia.org/wiki/Linux
+[make]: https://www.gnu.org/software/make/
+[module]: https://en.wikipedia.org/wiki/Loadable_kernel_module#Linux
---
+++ linux/linuxmod.c
@@ -0,0 +1,22 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+/* Module Information */
+MODULE_AUTHOR("Tyler Cromwell <tjc6185@gmail.com>");
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Example of a basic Linux kernel module");
+
+
+static int __init begin_module(void) {
+    printk(KERN_DEBUG "Hello world!\n");
+    return 0;
+}
+
+
+static void __exit end_module(void) {
+    printk(KERN_DEBUG "Goodbye, cruel world.\n");
+}
+
+
+module_init(begin_module);  /* insmod */
+module_exit(end_module);    /* rmmod */
