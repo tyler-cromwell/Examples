1b3b4919c07790c5b6cd2a537b6a86c6b7227008
Tyler Cromwell <tjc6185@gmail.com>
2019-04-01 22:30:52 -0400

    web: Not using html onclick attribute

--- web/index.js
+++ web/index.js
@@ -1,4 +1,20 @@
-function box(name) {
-    alert("Hello from "+ name); // Display alert box
-    event.stopPropagation();    // Prevents onclick event from bubbling up to parents
+window.onload = function() {
+    handler1 = function box(e) {
+        event.stopPropagation();    // Prevents onclick event from bubbling up to parents
+        alert("Hello from Box 1");  // Display alert box
+    }
+
+    handler2 = function box(e) {
+        event.stopPropagation();    // Prevents onclick event from bubbling up to parents
+        alert("Hello from Box 2");  // Display alert box
+    }
+
+    handler3 = function box(e) {
+        event.stopPropagation();    // Prevents onclick event from bubbling up to parents
+        alert("Hello from Box 3");  // Display alert box
+    }
+
+    document.getElementById("Box 1").addEventListener("click", handler1);
+    document.getElementById("Box 2").addEventListener("click", handler2);
+    document.getElementById("Box 3").addEventListener("click", handler3);
 }
