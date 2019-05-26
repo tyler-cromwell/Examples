69f7810d6fabe08ad16246376588adb6101702c4
Tyler Cromwell <tjc6185@gmail.com>
2019-05-25 22:25:18 -0400

    design_patterns: Expanded Singleton example

--- design_patterns/creational/singleton/Singleton.java
+++ design_patterns/creational/singleton/Singleton.java
@@ -1,11 +1,20 @@
 class Singleton {
     private static Singleton singleton = null;
-    private String data;
+    private String data = "";
 
     private Singleton() {}
 
     private Singleton(String parameter) {
         this.data = parameter;
+    }
+
+    @Override
+    public String toString() {
+        if (this.data == "" || this.data == null) {
+            return super.toString();
+        } else {
+            return this.data;
+        }
     }
 
     public static Singleton getInstance() {
@@ -17,13 +26,20 @@ class Singleton {
 
     public static Singleton getInstance(String parameter) {
         if (singleton == null) {
-            singleton = new Singleton();
+            singleton = new Singleton(parameter);
         }
         return singleton;
     }
 
     public static void main(String args[]) {
-        Singleton singleton = Singleton.getInstance();
+        Singleton singleton = null;
+
+        if (args.length > 0 && args[0] != null) {
+            singleton = Singleton.getInstance(args[0]);
+        } else {
+            singleton = Singleton.getInstance();
+        }
+
         System.out.println(singleton);
     }
 }
