05274498235c54296e16ffba9e4b968754ed8831
Tyler Cromwell <tjc6185@gmail.com>
2019-08-05 13:29:51 -0400

    web/javascript/event_bubbling: Simplified window.onload handler

--- web/javascript/event_bubbling/index.js
+++ web/javascript/event_bubbling/index.js
@@ -1,20 +1,20 @@
 window.onload = function() {
-    handler1 = function box(e) {
+    handler1 = function() {
         event.stopPropagation();    // Prevents onclick event from bubbling up to parents
         alert("Hello from Box 1");  // Display alert box
     }
 
-    handler2 = function box(e) {
+    handler2 = function() {
         event.stopPropagation();    // Prevents onclick event from bubbling up to parents
         alert("Hello from Box 2");  // Display alert box
     }
 
-    handler3 = function box(e) {
+    handler3 = function() {
         event.stopPropagation();    // Prevents onclick event from bubbling up to parents
         alert("Hello from Box 3");  // Display alert box
     }
 
-    document.getElementById("Box 1").addEventListener("click", handler1);
-    document.getElementById("Box 2").addEventListener("click", handler2);
-    document.getElementById("Box 3").addEventListener("click", handler3);
+    document.getElementById("Box 1").onclick = handler1;
+    document.getElementById("Box 2").onclick = handler2;
+    document.getElementById("Box 3").onclick = handler3;
 }
