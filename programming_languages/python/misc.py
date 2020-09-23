bf85dedf55f37ebe8e1678870811076b1f516e66
Tyler Cromwell <tjc6185@gmail.com>
2020-09-23 18:26:55 -0400

    python: Added example of how to print installed pip (v3) packages

--- programming_languages/python/misc.py
+++ programming_languages/python/misc.py
@@ -1,4 +1,5 @@
 import math
+from pip._internal.utils.misc import get_installed_distributions
 
 
 def round_up_to(value, alignment):
@@ -7,3 +8,8 @@ def round_up_to(value, alignment):
 
 def round_up_to(value, alignment):
     return alignment * math.floor(value / alignment)
+
+
+def get_installed_packages():
+    for package in get_installed_distributions():
+        print("{}: {}".format(package, time.ctime(os.path.getctime(package.location))))
