4328fd4b3d2a224274442e42470e91a6d813225b
Tyler Cromwell <tjc6185@gmail.com>
2021-08-14 14:16:25 -0400

    Added a Hello World HTTP server for Postgres index performance

---
+++ databases/postgres/indexes/go.mod
@@ -0,0 +1,3 @@
+module github.com/tyler-cromwell/Examples/databases/postgres/indexes
+
+go 1.13
---
+++ databases/postgres/indexes/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "os"
+)
+
+func main() {
+    http.HandleFunc("/", HelloServer)
+    http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
